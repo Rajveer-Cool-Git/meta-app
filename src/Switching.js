f149fe51955e7b8b0dab0c84868e0e7fea0fa9c4
Rajveer <rajveercoolplugins@gmail.com>
2023-08-24 18:29:09 +0530

    network switching as a list

--- src/Switching.js
+++ src/Switching.js
@@ -1,29 +1,67 @@
-import { useNetwork, useSwitchNetwork } from 'wagmi'
+import { useNetwork, useSwitchNetwork } from 'wagmi';
 import './App.css';
 
 function SwitchNet() {
-  const { chain } = useNetwork()
-  const { chains, error, isLoading, pendingChainId, switchNetwork } =
-    useSwitchNetwork()
+  const { chain } = useNetwork();
+  const { chains, error, isLoading, pendingChainId, switchNetwork } = useSwitchNetwork();
+
+  const handleNetworkChange = (event) => {
+    const selectedChainId = parseInt(event.target.value, 10);
+    switchNetwork?.(selectedChainId);
+  };
 
   return (
     <>
+
       {chain && <div>Connected to {chain.name}</div>}
-
-      {chains.map((x) => (
-        <button
-          disabled={!switchNetwork || x.id === chain?.id}
-          key={x.id}
-          onClick={() => switchNetwork?.(x.id)}
-          className='switchbtn'
-        >
-          {x.name}
-          {isLoading && pendingChainId === x.id && ' (switching)'}
-        </button>
-      ))}
-
+      <div className='box'>
+      <select className="switchSelect" onChange={handleNetworkChange}>
+        {chains.map((x) => (
+          <option
+            key={x.id}
+            value={x.id}
+            disabled={!switchNetwork || x.id === chain?.id}
+          >
+            {x.name}
+            {isLoading && pendingChainId === x.id && ' (switching)'}
+          </option>
+        ))}
+      </select>
+      </div>
       <div>{error && error.message}</div>
     </>
-  )
+  );
 }
- export default SwitchNet;
+
+export default SwitchNet;
+
+
+// import { useNetwork, useSwitchNetwork } from 'wagmi'
+// import './App.css';
+
+// function SwitchNet() {
+//   const { chain } = useNetwork()
+//   const { chains, error, isLoading, pendingChainId, switchNetwork } =
+//     useSwitchNetwork()
+
+//   return (
+//     <>
+//       {chain && <div>Connected to {chain.name}</div>}
+
+//       {chains.map((x) => (
+//         <button
+//           disabled={!switchNetwork || x.id === chain?.id}
+//           key={x.id}
+//           onClick={() => switchNetwork?.(x.id)}
+//           className='switchbtn'
+//         >
+//           {x.name}
+//           {isLoading && pendingChainId === x.id && ' (switching)'}
+//         </button>
+//       ))}
+
+//       <div>{error && error.message}</div>
+//     </>
+//   )
+// }
+//  export default SwitchNet;
